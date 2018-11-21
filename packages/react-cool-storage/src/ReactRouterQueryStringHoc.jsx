7ed54be9d9ac300f1d257617a98237001d76af00
Damien Clarke <dxinteractive@gmail.com>
2018-11-21 15:34:57 +1100

    feat: add super early draft of react router query string hoc

--- packages/react-cool-storage/src/ReactRouterQueryStringHoc.jsx
+++ packages/react-cool-storage/src/ReactRouterQueryStringHoc.jsx
@@ -3,81 +3,100 @@ import type {ComponentType} from 'react';
 import type {Node} from 'react';
 
 import React from 'react';
+import map from 'unmutable/lib/map';
+
+type Config = {
+    method: string,
+    name: string,
+    silent: boolean
+};
 
 type Props = {
-    // [config.name]?: Parcel
+    history: {
+        push: Function,
+        replace: Function
+    },
+    location: {
+        search: string
+    }
 };
+
 type ChildProps = {
     // [config.name]?: ReactCoolStorageMessage
 };
 
-type ReactRouterQueryStringHocConfig = {
-    name: string
-};
-
-export default (config: ReactRouterQueryStringHocConfig): Function => {
+export default (config: Config): Function => {
     let {
-        name
+        method = "push",
+        name,
+        silent = false
     } = config;
 
     return (Component: ComponentType<ChildProps>) => class ReactRouterQueryStringHoc extends React.Component<Props> {
+
+        checkAvailable = () => {
+            if(typeof URLSearchParams === "undefined") {
+                throw new Error(`ReactRouterQueryStringHoc requires URLSearchParams to be defined`);
+            }
+
+            let {
+                history,
+                location
+            } = this.props;
+
+            if(!history || !location) {
+                throw new Error(`ReactRouterQueryStringHoc requires React Router history and location props`);
+            }
+        };
+
+        handleChange = (newValue: *) => {
+            let {history} = this.props;
+            let searchParams = new URLSearchParams();
+
+            map((value, key) => {
+                searchParams.set(key, value);
+            })(newValue);
+
+            history[method]("?" + searchParams.toString());
+        }
+
         render(): Node {
 
-            // let fromProps = (value) => (typeof value === "function") ? value(this.props) : value;
+            let message = {
+                value: {},
+                onChange: () => {},
+                available: false
+            };
 
-            // // $FlowFixMe - flow can'tm seem to understand that name will never be a boolean or number according to its own previous types
-            // let name: string = fromProps(config.name);
-            // // $FlowFixMe
-            // let debounce: ?number = fromProps(config.debounce) || undefined;
-            // // $FlowFixMe
-            // let hold: boolean = fromProps(config.hold) || false;
-            // // $FlowFixMe
-            // let originalParcelProp: ?string = fromProps(config.originalParcelProp);
-            // let debugBuffer: boolean = config.debugBuffer || false;
-            // let debugParcel: boolean = config.debugParcel || false;
+            try {
+                this.checkAvailable();
 
-            // Types(PARCEL_BOUNDARY_HOC_NAME, "config.name", "string")(name);
-            // debounce && Types(PARCEL_BOUNDARY_HOC_NAME, "config.debounce", "number")(debounce);
-            // Types(PARCEL_BOUNDARY_HOC_NAME, "config.hold", "boolean")(hold);
-            // Types(PARCEL_BOUNDARY_HOC_NAME, "config.debugBuffer", "boolean")(debugBuffer);
-            // Types(PARCEL_BOUNDARY_HOC_NAME, "config.debugParcel", "boolean")(debugParcel);
-            // originalParcelProp && Types(PARCEL_BOUNDARY_HOC_NAME, "config.originalParcelProp", "string")(originalParcelProp);
+                let query = {};
+                let searchParams = new URLSearchParams(location.search);
+                for (let [key, value] of searchParams) {
+                    query[key] = value;
+                }
 
-            // let parcel = this.props[name];
-            // if(!parcel) {
-            //     return <Component {...this.props} />;
-            // }
+                message = {
+                    value: query,
+                    onChange: this.handleChange,
+                    available: true
+                };
 
-            // Types(`ParcelBoundaryHoc()`, `prop "${name}"`, `parcel`)(parcel);
+            } catch(e) {
+                if(!silent) {
+                    throw e;
+                }
+            }
 
-            // return <ParcelBoundary
-            //     parcel={parcel}
-            //     debounce={debounce}
-            //     hold={hold}
-            //     debugBuffer={debugBuffer}
-            //     debugParcel={debugParcel}
-            //     pure={false}
-            // >
-            //     {(innerParcel, actions, buffered) => {
-            //         let childProps = {
-            //             ...this.props,
-            //             // $FlowFixMe - I want to use a computed property, flow
-            //             [name]: innerParcel,
-            //             // $FlowFixMe - I want to use a computed property, flow
-            //             [name + "Actions"]: actions,
-            //             // $FlowFixMe - I want to use a computed property, flow
-            //             [name + "Buffered"]: buffered
-            //         };
+            let childProps = {
+                [name]: message
+            };
 
-            //         if(originalParcelProp) {
-            //             childProps[originalParcelProp] = parcel;
-            //         }
-
-            //         return <Component {...childProps} />;
-            //     }}
-            // </ParcelBoundary>;
-
-            return <Component { ...this.props} />;
+            return <Component
+                {...this.props}
+                {...childProps}
+            />;
         }
     };
 };
