ff1e005f15558bdf2ec379ce532d679bcd256aa7
Damien Clarke <dxinteractive@gmail.com>
2018-11-23 10:11:52 +1100

    feat: add WebStorageHoc

--- packages/react-cool-storage/src/WebStorageHoc.jsx
+++ packages/react-cool-storage/src/WebStorageHoc.jsx
@@ -1,157 +1,89 @@
-// // @flow
-// import type {ComponentType} from 'react';
-// import type {Node} from 'react';
+// @flow
+import ReactCoolStorageHoc from './ReactCoolStorageHoc';
 
-// import React from 'react';
+import pipeWith from 'unmutable/lib/pipeWith';
 
-// import ReactCoolStorageMessage from './ReactCoolStorageMessage';
+type Config = {
+    name: string,
+    key: string,
+    method?: string,
+    silent?: boolean,
+    reconstruct?: Function,
+    deconstruct?: Function,
+    parse?: (data: string) => any,
+    stringify?: (data: any) => string
+};
 
-// import forEach from 'unmutable/lib/forEach';
-// import identity from 'unmutable/lib/identity';
-// import isKeyed from 'unmutable/lib/isKeyed';
-// import pipeWith from 'unmutable/lib/pipeWith';
+type Props = {
+    history: {
+        push: Function,
+        replace: Function
+    },
+    location: {
+        search: string
+    }
+};
 
-// type Config = {
-//     afterParse?: Function,
-//     beforeStringify?: Function,
-//     method?: string,
-//     name: string,
-//     key: string,
-//     parse?: (data: string) => any,
-//     silent?: boolean,
-//     stringify?: (data: any) => string
-// };
+const hoc = "WebStorageHoc";
 
-// type Props = {
-//     history: {
-//         push: Function,
-//         replace: Function
-//     },
-//     location: {
-//         search: string
-//     }
-// };
+export default (config: Config): Function => {
+    let {
+        key,
+        method = "localStorage",
+        parse = (data: string): any => JSON.parse(data),
+        stringify = (data: any): string => JSON.stringify(data)
+    } = config;
 
-// type ChildProps = {
-//     // [config.name]?: ReactCoolStorageMessage
-// };
+    if(typeof key !== "string") {
+        throw new Error(`${hoc} expects param "config.key" to be a string, but got ${typeof key}`);
+    }
 
-// export default (config: Config): Function => {
-//     let {
-//         afterParse = identity(),
-//         beforeStringify = identity(),
-//         method = "localStorage",
-//         name,
-//         key,
-//         parse = (data: string): any => JSON.parse(data),
-//         silent = false,
-//         stringify = (data: any): string => JSON.stringify(data)
-//     } = config;
+    if(method !== "localStorage" && method !== "sessionStorage") {
+        throw new Error(`${hoc} expects param "config.method" to be either "localStorage" or "sessionStorage"`);
+    }
 
-//     if(typeof name !== "string") {
-//         throw new Error(`WebStorageHoc() expects param "config.name" to be a string, but got undefined`);
-//     }
+    let storage = window[method];
+    let getValue = () => parse(storage.getItem(key)) || {};
 
-//     if(typeof key !== "string") {
-//         throw new Error(`WebStorageHoc() expects param "config.key" to be a string, but got undefined`);
-//     }
+    return ReactCoolStorageHoc({
+        hoc,
+        config,
+        checkAvailable: (/*props: Props*/): ?string => {
+            let x = '__storage_test__';
 
-//     if(method !== "localStorage" && method !== "sessionStorage") {
-//         throw new Error(`ReactRouterQueryStringHoc() expects param "config.method" to be either "localStorage" or "sessionStorage"`);
-//     }
+            try {
+                storage.setItem(x, x);
+                storage.removeItem(x);
 
-//     let storage = window[method];
+            } catch(e) {
+                /* istanbul ignore next */
+                // $FlowFixMe - flow doesnt know about DOMException
+                let available = e instanceof DOMException && (
+                    // everything except Firefox
+                    e.code === 22 ||
+                    // Firefox
+                    e.code === 1014 ||
+                    // test name field too, because code might not be present
+                    // everything except Firefox
+                    e.name === 'QuotaExceededError' ||
+                    // Firefox
+                    e.name === 'NS_ERROR_DOM_QUOTA_REACHED') &&
+                    // acknowledge QuotaExceededError only if there's something already stored
+                    storage.length !== 0;
 
-//     return (Component: ComponentType<ChildProps>) => class WebStorageHoc extends React.Component<Props> {
-
-//         checkAvailable = () => {
-
-//             let x = '__storage_test__';
-
-//             try {
-//                 storage.setItem(x, x);
-//                 storage.removeItem(x);
-
-//             } catch(e) {
-//                 // $FlowFixMe - flow doesnt know about DOMException
-//                 let available = e instanceof DOMException && (
-//                     // everything except Firefox
-//                     e.code === 22 ||
-//                     // Firefox
-//                     e.code === 1014 ||
-//                     // test name field too, because code might not be present
-//                     // everything except Firefox
-//                     e.name === 'QuotaExceededError' ||
-//                     // Firefox
-//                     e.name === 'NS_ERROR_DOM_QUOTA_REACHED') &&
-//                     // acknowledge QuotaExceededError only if there's something already stored
-//                     storage.length !== 0;
-
-//                 if(!available) {
-//                     throw new Error(`WebStorageHoc requires ${method} to be available.`);
-//                 }
-//             }
-//         };
-
-//         handleChange = (newValue: *) => {
-//             if(!isKeyed(newValue)) {
-//                 throw new Error(`WebStorageHoc onChange must be passed an object`);
-//             }
-
-//             pipeWith(
-//                 newValue,
-//                 beforeStringify,
-//                 // forEach((value, key) => {
-//                 //     if(typeof value === "undefined") {
-//                 //         searchParams.delete(key);
-//                 //     } else {
-//                 //         searchParams.set(key, stringify(value));
-//                 //     }
-//                 // })
-//             );
-
-//             //history[method]("?" + searchParams.toString());
-//         }
-
-//         render(): Node {
-
-//             let message = ReactCoolStorageMessage.unavailable();
-//             let valid = true;
-
-//             try {
-//                 this.checkAvailable();
-
-//                 let query = {};
-//                 // let searchParams = this.getSearchParams();
-//                 // for (let [key, value] of searchParams) {
-//                 //     try {
-//                 //         query[key] = parse(value);
-//                 //     } catch(e) {
-//                 //         valid = false;
-//                 //     }
-//                 // }
-
-//                 message = new ReactCoolStorageMessage({
-//                     available: true,
-//                     onChange: this.handleChange,
-//                     valid,
-//                     value: valid ? afterParse(query) : {}
-//                 });
-
-//             } catch(e) {
-//                 if(!silent) {
-//                     throw e;
-//                 }
-//             }
-
-//             let childProps = {
-//                 [name]: message
-//             };
-
-//             return <Component
-//                 {...this.props}
-//                 {...childProps}
-//             />;
-//         }
-//     };
-// };
+                if(!available) {
+                    return `WebStorageHoc requires ${method} to be available.`;
+                }
+            }
+        },
+        getValue,
+        handleChange: (props: Props, {update}: *) => {
+            pipeWith(
+                getValue(),
+                update,
+                stringify,
+                (str: string) => storage.setItem(key, str)
+            );
+        }
+    });
+};
