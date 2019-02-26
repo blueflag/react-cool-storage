d9523e3cee6dbbf4d7bd146f18cd88aa4fbfec0d
Damien Clarke <dxinteractive@gmail.com>
2019-02-26 14:02:13 +1100

    docs: roll back to use mdxc so we can pass props into mdx

--- packages/react-cool-storage-docs/gatsby-config.js
+++ packages/react-cool-storage-docs/gatsby-config.js
@@ -7,6 +7,14 @@ module.exports = {
     plugins: [
         'gatsby-plugin-sass',
         'gatsby-plugin-react-helmet',
-        'gatsby-plugin-offline'
+        'gatsby-plugin-offline',
+        {
+            resolve: 'gatsby-transformer-remark',
+            options: {
+                plugins: [
+                    `gatsby-remark-prismjs`
+                ]
+            }
+        }
     ]
 };
