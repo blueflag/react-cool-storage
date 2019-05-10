31a1b92a8642abc4ea4daee6019de2e996c42a9e
Damien Clarke <dxinteractive@gmail.com>
2019-05-10 11:09:11 +1000

    docs: remove examples until symlinked hooks work

--- packages/react-cool-storage-docs/src/example/WebStorageExample.jsx
+++ packages/react-cool-storage-docs/src/example/WebStorageExample.jsx
@@ -1,40 +1,42 @@
 // @flow
 import React from 'react';
-import ReactCoolStorageHoc from 'react-cool-storage';
-import WebStorage from 'react-cool-storage/WebStorage';
+// import ReactCoolStorageHoc from 'react-cool-storage';
+// import WebStorage from 'react-cool-storage/WebStorage';
 
-const WebStorageExample = (props) => {
-    let {webStorage} = props;
-    return <div>
-        <label>localStorage data stored under a key of "exampleStorage.foo"</label>
-        <input
-            value={webStorage.value.foo || ""}
-            onChange={(event) => webStorage.onChange({
-                foo: event.currentTarget.value
-            })}
-        />
-        <label>localStorage data stored under a key of "exampleStorage.bar"</label>
-        <input
-            value={webStorage.value.bar || ""}
-            onChange={(event) => webStorage.onChange({
-                bar: event.currentTarget.value
-            })}
-        />
-    </div>;
-};
+// const WebStorageExample = (props) => {
+//     let {webStorage} = props;
+//     return <div>
+//         <label>localStorage data stored under a key of "exampleStorage.foo"</label>
+//         <input
+//             value={webStorage.value.foo || ""}
+//             onChange={(event) => webStorage.onChange({
+//                 foo: event.currentTarget.value
+//             })}
+//         />
+//         <label>localStorage data stored under a key of "exampleStorage.bar"</label>
+//         <input
+//             value={webStorage.value.bar || ""}
+//             onChange={(event) => webStorage.onChange({
+//                 bar: event.currentTarget.value
+//             })}
+//         />
+//     </div>;
+// };
 
-export default ReactCoolStorageHoc(
-    'webStorage',
-    WebStorage({
-        key: 'exampleStorage'
-    })
-)(WebStorageExample);
+// export default ReactCoolStorageHoc(
+//     'webStorage',
+//     WebStorage({
+//         key: 'exampleStorage'
+//     })
+// )(WebStorageExample);
 
-// also add a WebStorage instance to the window
-// to demonstrate usage outside of React
+// // also add a WebStorage instance to the window
+// // to demonstrate usage outside of React
 
-if(typeof window !== "undefined") {
-    window.MyWebStorage = WebStorage({
-        key: 'exampleStorage'
-    });
-}
+// if(typeof window !== "undefined") {
+//     window.MyWebStorage = WebStorage({
+//         key: 'exampleStorage'
+//     });
+// }
+
+export default () => <div>Exmaple goes here</div>;
