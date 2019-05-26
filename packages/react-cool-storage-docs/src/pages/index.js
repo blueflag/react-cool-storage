bb25020ca9e8cf4666c4d9c13e146dafa8429d21
Damien Clarke <dxinteractive@gmail.com>
2019-05-26 12:46:07 +1000

    docs: add logo

--- packages/react-cool-storage-docs/src/pages/index.js
+++ packages/react-cool-storage-docs/src/pages/index.js
@@ -2,19 +2,22 @@
 import React from 'react';
 
 import {DocsHeader} from 'dcme-style';
+import {Image} from 'dcme-style';
 import {Link as HtmlLink} from 'dcme-style';
 import {Text} from 'dcme-style';
 
 import {Link} from 'dcme-gatsby';
 import ContentNav from '../shape/ContentNav';
 import Layout from '../layout/Layout';
-import IndexMarkdown from './indexMdx.mdx';
+import IndexMarkdown from './index.mdx';
+import Logo from 'assets/react-cool-storage-inverted.gif';
 
 export default () => <Layout>
     <DocsHeader
         title={() => <Text element="h1" modifier="sizeTera superDuper margin">react-cool-storage</Text>}
         description={() => "React hooks and hocs with a common API for storing state outside of React. Query string, local storage etc. 😎"}
         links={() => <Text><HtmlLink href="https://github.com/blueflag/react-cool-storage">github</HtmlLink> | <HtmlLink href="https://www.npmjs.com/package/react-cool-storage">npm</HtmlLink> | <Link to="/api">api documentation</Link></Text>}
+        logo={Logo}
     />
     <ContentNav
         content={() => <IndexMarkdown />}
